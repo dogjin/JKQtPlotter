3c2b77656f1e3df674291a1ca77299f206a3e865
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-12 08:58:53 +0000

    Deploy jkriege2/JKQtPlotter to github.com/jkriege2/JKQtPlotter.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,112 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "JKQTPlotter", "index.html", [
+    [ "JKQTPlotter - A Qt Plotting Library", "index.html", [
+      [ "Main Features", "index.html#jkqtp_main_features", null ],
+      [ "Table Of Contents", "index.html#jkqtplottertic", null ]
+    ] ],
+    [ "How to Build", "d3/dce/a03608.html", [
+      [ "Build using QMake", "d3/dce/a03608.html#BUILDINSTRUCTIONS_QMAKE", [
+        [ "QMake Include Project", "d3/dce/a03608.html#BUILDINSTRUCTIONS_QMAKEINCLUDE", null ],
+        [ "QMake Static Library", "d3/dce/a03608.html#BUILDINSTRUCTIONS_QMAKESTATIC", null ],
+        [ "QMake Dynamic Library", "d3/dce/a03608.html#BUILDINSTRUCTIONS_QMAKEDYNAMIC", null ]
+      ] ]
+    ] ],
+    [ "Example & Tutorial Projects", "d8/d13/a03609.html", "d8/d13/a03609" ],
+    [ "Licensing Terms for JKQTPlotter", "df/d0b/a03610.html", [
+      [ "JKQTPlotter/JKQTFastPlotter/JKQTMathText: LGPL >= 2.1", "df/d0b/a03610.html#licensesec", null ],
+      [ "Licenses of 3rd Party Products Contained in JKQTPlotter", "df/d0b/a03610.html#extlicense_sec", [
+        [ "XITS Fonts for JKQTMathText", "df/d0b/a03610.html#extlicense_xits", null ]
+      ] ]
+    ] ],
+    [ "What's New (Release Notes/Versions)", "d0/d50/a03611.html", [
+      [ "trunk: Major Update/Refactoring", "d0/d50/a03611.html#WHATSNEW_TRUNK", [
+        [ "trunk: Overview", "d0/d50/a03611.html#WHATSNEW_TRUNK_OVERVIEW", null ],
+        [ "trunk: Download", "d0/d50/a03611.html#WHATSNEW_TRUNK_DOWNLOAD", null ]
+      ] ],
+      [ "v2018.08: Minor Modifications (Final QuickFit3-compatible Version)", "d0/d50/a03611.html#WHATSNEW_V2018_08", [
+        [ "v2018.08: Overview", "d0/d50/a03611.html#WHATSNEW_V2018_08_OVERVIEW", null ],
+        [ "v2018.08: Download", "d0/d50/a03611.html#WHATSNEW_V2018_08_DOWNLOAD", null ]
+      ] ],
+      [ "v2015.10: Initial Release", "d0/d50/a03611.html#WHATSNEW_V2015_10", [
+        [ "v2015.10: Overview", "d0/d50/a03611.html#WHATSNEW_V2015_10_OVERVIEW", null ],
+        [ "v2015.10: Download", "d0/d50/a03611.html#WHATSNEW_V2015_10_DOWNLOAD", null ]
+      ] ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/de5/a03279.html#aaefc2817876471be26b6553c58a95d1b",
+"d1/d45/a03191.html",
+"d2/de4/a03047.html#ad5384b2713d20534e36692d41d31e1a5",
+"d3/df5/a02827.html#a45d1ca361af4c483bc4f8d199fc97ec4",
+"d4/d90/a03355.html#ac392f5225980224a0802050753e8edbe",
+"d5/d25/a02687.html#ace24ff5fb312ed00729a17ebcfa1bd1b",
+"d5/d55/a02719.html#af7c234abec32842355a98a391818d582a13dd0ebb7c8812123f1868915a4c3936",
+"d6/d97/a03011.html#a271cef0fbd19cd6a93c0a7b1d0d8a316",
+"d6/df9/a02923.html#af3c7dedf4f4c296c5f562c1181db99fc",
+"d7/d5a/a02811.html#ab7d52569e02850fd222d654d1e42293d",
+"d8/d9c/a02070.html#ggaa94a0bca376a39f32a47dbd6f08fc594a02540a9b1538d3ae89ab37061cac3fc5",
+"d9/daf/a02807.html#a12384ea828821eb74d29620cda28c0f8",
+"d9/daf/a02807.html#aa04b1ccb812be9bac34bd9c952f62e59",
+"da/d0d/a02955.html#a336daf7b22c725b057378d5b25a08aa4",
+"da/dcd/a02987.html#ae5c6441f28db819cebf9f4b1360d7e00",
+"db/d65/a03179.html#aab8ccba161a7795c07d48973c5803d32",
+"dc/d58/a02667.html#a2142b8edc1c1e2e15d618a0b2f55246e",
+"dc/d94/a02999.html",
+"dd/d4e/a02971.html#a17e325abb53c01910c6c7dcb2655b660",
+"de/d11/a03267.html#a71602469019acad64755c2938fc6a41d",
+"de/d9d/a02072.html#ggafba2dfd5e979b2a4c063ac2316ce42f1aeb2d5d16ed32bc6829f015665bb27d34",
+"df/d74/a02931.html#ab8e2dc047084279f75b8871c9b3ab8c3"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
